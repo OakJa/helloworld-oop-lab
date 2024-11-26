8dc2c31e5010790cf00f01c7c4d271a924e89dba
OakJa <phutthiphong.n@kkumail.com>
2024-11-26 11:18:01 +0700

    Create Program.cs

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Phutthiphong Nhamjatthurat";
+string id = "673450038-0";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("my name is Phutthiphong Nhamjatthurat");
+Console.WriteLine("my student id:673450038-0");
+Console.WriteLine("my name is " + name); 
+Console.WriteLine("my studen id:" + id);
